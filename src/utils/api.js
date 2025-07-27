199db67b3b5a943dd6137203a885c19f942a082b
hyunjoo.kim <khjoo1203@gmail.com>
2025-07-27 13:38:18 +0900

    cors문제 해결

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,6 +8,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // CORS credentials 지원
 });
 /**
  * console.log all requests and responses
