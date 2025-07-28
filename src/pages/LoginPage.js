2495cbd9f141900f863cab28c2759c503fee3178
hyunjoo <khjoo1203@gmail.com>
2025-07-28 20:05:28 +0900

    오류 수정하기

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,7 +9,6 @@ const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
   const handleLogin = async (event) => {
@@ -17,13 +16,11 @@ const LoginPage = () => {
     try {
       const response = await api.post('/user/login', { email, password });
       if (response.status === 200) {
-        setUser(response.data.user);
         sessionStorage.setItem('token', response.data.token);
         api.defaults.headers['authorization'] = 'Bearer ' + response.data.token;
         setError('');
         navigate('/');
       }
-      throw new Error(response.message);
     } catch (error) {
       setError(error.message);
     }
