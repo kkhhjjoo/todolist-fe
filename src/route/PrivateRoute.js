948f49e3d7614a77187fabce9b2d295e18216168
hyunjoo <khjoo1203@gmail.com>
2025-07-29 11:56:06 +0900

    토큰

---
+++ src/route/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Navigate} from 'react-router-dom';
+
+const PrivateRoute = ({user,children}) => {
+    
+  return (
+    <div>
+      user? children: <Navigate to = '/login' />;
+    </div>
+  )
+}
+
+export default PrivateRoute
